992179b3ea5f0922554f82c8e9f9a9aef54f5feb
@hexlet/cli <support@hexlet.io>
2023-01-06 14:01:20 +0100

    submit java-collections-ru/streams

---
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.util.List;
+// BEGIN
+public class App {
+    public static long getCountOfFreeEmails(List<String> emails) {
+        List<String> freeEmails = List.of("yandex.ru", "gmail.com", "hotmail.com");
+        return emails.stream()
+                .map(email -> email.split("@")[1])
+                .filter(email -> freeEmails.contains(email))
+                .count();
+    }
+}
+// END
