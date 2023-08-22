e4186d52cb0af6ec82a3eead41d680514dd565a4
@hexlet/cli <support@hexlet.io>
2023-08-22 21:00:44 +0200

    submit java-spring-ru/trees

--- java-spring-ru/trees/src/main/java/exercise/controller/CourseController.java
+++ java-spring-ru/trees/src/main/java/exercise/controller/CourseController.java
@@ -7,12 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/courses")
